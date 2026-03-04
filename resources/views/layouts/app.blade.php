1306fe0ed7575073cbb173abbbb747636880b942
sukhpreet <163695962+roninPb65@users.noreply.github.com>
2026-03-04 15:24:52 -0700

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,6 +12,7 @@
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800;900&display=swap"
         rel="stylesheet">
+<link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
     <style>
         * {
